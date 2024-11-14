66eb1a6aa7be3a3e44a5e0cf7215d7e6e04cd94e
Ginga Kennis <gingakennis123@icloud.com>
2024-11-14 14:31:10 +0900

    change threshould for grid_to_map_cloud

--- robot_helpers/conversions.py
+++ robot_helpers/conversions.py
@@ -7,7 +7,7 @@ def map_cloud_to_grid(voxel_size, points, values):
     return voxel
 
 
-def grid_to_map_cloud(voxel_size, voxel, threshold=1e-2):
-    points = np.argwhere(voxel > threshold) * voxel_size
-    values = np.expand_dims(voxel[voxel > threshold], 1)
+def grid_to_map_cloud(voxel_size, voxel, threshold=0.0):
+    points = np.argwhere(voxel >= threshold) * voxel_size
+    values = np.expand_dims(voxel[voxel >= threshold], 1)
     return points, values
